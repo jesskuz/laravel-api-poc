b3cbfb63ff8151985fd47b140fbdd3d432d1138b
Jesse Kuczynski <jesskuz@users.noreply.github.com>
2024-01-04 01:32:21 -0600

    enumerating api routes using apiResource

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TeamController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+
+Route::apiResource('teams', TeamController::class);
+Route::apiResource('members', MemberController::class);
+Route::apiResource('projects', ProjectController::class);
