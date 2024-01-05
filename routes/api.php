442d1cb2ca2dc5de3a8c07b6a99dfe1b7690dd3e
Jesse Kuczynski <jesskuz@users.noreply.github.com>
2024-01-04 23:52:56 -0600

    add custom routes before apiResource is called

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TeamController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,8 +17,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-
+// Update the team of a member
+Route::put('/members/{member}/update-team', [MemberController::class, 'updateTeam']);
+// Get the members of a specific team
+Route::get('/teams/{team}/get-members', [TeamController::class, 'getMembers']);
+// Add a member to a project
+Route::put('/projects/{project}/add-member', [ProjectController::class, 'addMember']);
+// Get the members of a specific project
+Route::get('/projects/{project}/get-members', [ProjectController::class, 'getMembers']);
 
 Route::apiResource('teams', TeamController::class);
 Route::apiResource('members', MemberController::class);
 Route::apiResource('projects', ProjectController::class);
+
+Route::fallback(function () {
+    return response()->json([
+        'message' => 'invalid path'
+    ], 404);
+});
