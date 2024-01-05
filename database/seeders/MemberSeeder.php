c1f344f14b745d77e848b08835e3de1ff6d7ff0c
Jesse Kuczynski <jesskuz@users.noreply.github.com>
2024-01-05 15:09:24 -0600

    remove unused import in seeders

--- database/seeders/MemberSeeder.php
+++ database/seeders/MemberSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Member;
 use App\Models\Project;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
